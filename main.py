9ff867d52530c5ac21884572bf438110ba628b47
ghost <kirill.han011@gmail.com>
2022-04-18 23:12:45 +0600

    Добавлена проверка на износ инструмента у робота

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
+class MarginOfSafetyException(Exception):
+    pass
+
 class Tool:
     def __init__(self, name, margin_of_safety=100) -> None:
         self.name = name
         self.margin_of_safety = margin_of_safety
 
     def action(self):
+        if self.margin_of_safety < 0:
+            self.margin_of_safety = 0
+            self.__is_still_work()
         self.margin_of_safety -= 10
+
+    def __is_still_work(self):
+        if self.margin_of_safety == 0:
+            raise MarginOfSafetyException(f"Инструмент {self.name} изношен")
 
 
 class Saw(Tool):
     def action(self):
-        super().action()
         print("взззззвзззз")
+        super().action()
 
 
 class Drill(Tool):
     def action(self):
-        super().action()
         print("дрдрддррр")
+        super().action()
 
 
 class Hammer(Tool):
     def action(self):
-        super().action()
         print("бам-бам")
+        super().action()
 
 
 class Robot:
