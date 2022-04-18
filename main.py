59d1f1299438dcdf328d52ba9f63f07698c95bba
ghost <kirill.han011@gmail.com>
2022-04-18 23:38:36 +0600

    Добавлена возможность пользоваться не одним инструментом

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 class MarginOfSafetyException(Exception):
     pass
 
+
+class SlotsCountException(Exception):
+    pass
+
+
 class Tool:
     def __init__(self, name, margin_of_safety=100) -> None:
         self.name = name
@@ -36,26 +41,50 @@ class Hammer(Tool):
 
 
 class Robot:
-    def __init__(self, name) -> None:
+    def __init__(self, name, tools_slots_count) -> None:
         self.name = name
-        self.tool = None
+        self.tools_slots = []
+        self.tools_slots_count = tools_slots_count
 
     def setup_tool(self, tool):
-        if self.tool:
-            print(f"Уже установлен интрумент: {self.name}")
+        if self.tools_slots_count:
+            self.tools_slots.append(tool)
+            self.tools_slots_count -= 1
         else:
-            self.tool = tool
+            raise SlotsCountException("Слоты заклнчились")
 
-    def drop_tool(self):
-        if not self.tool:
-            print("Не один инструмент не установлен")
+    def drop_tool(self, tool):
+        if tool:
+            self.tools_slots.remove(tool)
         else:
-            tool = self.tool
-            self.tool = None
-            return tool
+            print("Нет такого инструмента")
 
     def action(self):
-        if not self.tool:
+        if not self.tools_slots:
             print("Нечем работать")
         else:
-            self.tool.action()
+            for tool in self.tools_slots:
+                try:
+                    tool.action()
+                except MarginOfSafetyException as e:
+                    print(e)
+                    self.drop_tool(tool)
+
+
+saw = Saw("pila")
+drill = Drill("drel")
+hammer = Hammer("Molotok")
+robot = Robot("Vasya")
+
+tools = [saw, drill, hammer]
+
+for tool in tools:
+    robot.setup_tool(tool)
+    for _ in range(11):
+        try:
+            robot.action()
+        except MarginOfSafetyException as e:
+            print(e)
+            robot.drop_tool()
+            break
+    robot.drop_tool()
