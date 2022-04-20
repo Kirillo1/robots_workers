8e0255b60e917e977f2879c460fad4a731b229da
ghost <kirill.han011@gmail.com>
2022-04-21 00:03:18 +0600

    добавлен вес инструмента

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ class SlotsCountException(Exception):
     pass
 
 
+class WeightException(Exception):
+    pass
+
+
 class Tool:
-    def __init__(self, name, margin_of_safety=100) -> None:
+    def __init__(self, name, weight, margin_of_safety=100) -> None:
         self.name = name
+        self.weight = weight
         self.margin_of_safety = margin_of_safety
 
     def action(self):
@@ -26,7 +31,7 @@ class Tool:
             raise MarginOfSafetyException(f"Инструмент {self.name} изношен")
 
     def display(self):
-        print(f"Название: {self.name}...прочность: {self.margin_of_safety}")
+        print(f"Название: {self.name}...прочность: {self.margin_of_safety}...вес: {self.weight}")
 
 
 class Saw(Tool):
@@ -52,12 +57,16 @@ class Robot:
         self.name = name
         self.tools_slots = []
         self.tools_slots_count = tools_slots_count
+        self.weight_limit = randint(1, 30)
         print(f"Имя робота: {self.name} - слотов: {self.tools_slots_count}")
 
     def setup_tool(self, tool):
+        if self.weight_limit - tool.weight < 0:
+            raise WeightException(f"Превышен лимит по весу")
         if self.tools_slots_count:
             self.tools_slots.append(tool)
             self.tools_slots_count -= 1
+            self.weight_limit -= tool.weight
         else:
             raise SlotsCountException("Слоты заклнчились")
 
@@ -79,38 +88,28 @@ class Robot:
                     self.drop_tool(tool)
 
 
-saw = Saw("pila")
-drill = Drill("drel")
-hammer = Hammer("Molotok")
-saw2 = Saw("pila2")
-drill2 = Drill("drel2")
-hammer2 = Hammer("Molotok2")
+saw = Saw("pila", randint(3, 10), randint(80, 120))
+drill = Drill("drel", randint(3, 1010), randint(80, 120))
+hammer = Hammer("Molotok", randint(3, 10), randint(80, 120))
+saw2 = Saw("pila2", randint(3, 10), randint(80, 120))
+drill2 = Drill("drel2", randint(3, 10), randint(80, 120))
+hammer2 = Hammer("Molotok2", randint(3, 10), randint(80, 120))
 
-
-robot = Robot("Vasya", randint(1,4))
+robot = Robot("Vasya", randint(1, 4))
 
 tools = [saw, drill, hammer, saw2, drill2, hammer2]
 shuffle(tools)
 
-for tool in tools:
+while robot.tools_slots_count or tools:
+    tool = tools.pop(randint(0, len(tools) - 1))
     try:
         robot.setup_tool(tool)
     except SlotsCountException as e:
         print(e)
         break
+    except WeightException as e:
+        print(e)
+        break
 
 while robot.tools_slots:
     robot.action()
-
-
-
-# for tool in tools:
-#     robot.setup_tool(tool)
-#     for _ in range(11):
-#         try:
-#             robot.action()
-#         except MarginOfSafetyException as e:
-#             print(e)
-#             robot.drop_tool()
-#             break
-#     robot.drop_tool()
